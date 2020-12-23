6bdfed0a93dba88569a8e09dc523578a1182b84c
Luana Silveira e Silva Schlei <luana.schlei@gmail.com>
2020-12-23 16:30:58 +0100

    Pong 1
    - Virtual screen like in NES
    - add escape key
    - push apply (start, end)
    - set default filter

--- main.lua
+++ main.lua
@@ -1,14 +1,33 @@
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+push = require 'push'
+
+--[[
+    isso é um comentario
+]]
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         vsync = true,
         resizable = false
     })
 end
 
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
 function love.draw()
-    love.graphics.print("Hello Pong!", WINDOW_WIDTH / 2 - 34, WINDOW_HEIGHT / 2 - 6)
+    push:apply('start')
+
+    love.graphics.print("Hello Pong!", VIRTUAL_WIDTH / 2 - 34, VIRTUAL_HEIGHT / 2 - 6)
+
+    push:apply('end')
 end
